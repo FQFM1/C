82a6c3c71d7c7b53025abaf36eadf65fff75f168
FQFM1 <lizhaobo1998117@gmail.com>
2024-11-11 14:51:09 +0800

    Chap 5

---
+++ Chapter_5/main.c
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	char arr1[] = "welcome to bit...";
+//	char arr2[] = "#################";
+//	int left = 0;
+//	int right = strlen(arr1) - 1;
+//	printf("%s\n", arr2);
+//	while (left <= right)
+//	{
+//		Sleep(1000);
+//		arr2[left] = arr1[left];
+//		arr2[right] = arr1[right];
+//		left++;
+//		right--;
+//		printf("%s\n", arr2);
+//	}
+//	return 0;
+//}
+
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int left = 0;
+	int right = sizeof(arr) / sizeof(arr[0]) - 1;
+	int key = 7;
+	//要找的数字
+	int mid = 0;
+	//记录中间元素的下标
+	int find = 0;
+	while (left <= right)
+	{
+		mid = (left + right) / 2;
+		if (arr[mid] > key)
+		{
+			right = mid - 1;
+		}
+		else if (arr[mid] < key)
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			find = 1;
+			break;
+		}
+	}
+	if (1 == find)
+		printf("找到了,下标是%d\n", mid);
+	else
+		printf("找不到\n");
+}
